42f1e79633aa2f39354cfa0d5b904bf1bc75f274
chailunneng <577101892@qq.com>
2018-04-17 11:45:11 +0800

    增加初始登陆页面

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './system/login/login.component';
+import {AppRouteModule} from './app-route.module';
+import {NofoundComponent} from './common/nofound/nofound.component';
+import {FormBuilder, FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NofoundComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouteModule,
+    FormsModule,
+    ReactiveFormsModule,
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [
+    FormBuilder,
+  ],
+  exports: [
+    ReactiveFormsModule,
+  ]
 })
 export class AppModule { }
